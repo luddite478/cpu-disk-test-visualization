e385d5350b33ca2fda3dc4596980437680a0da2a
Roman <roman.smirnov@vicuesoft.com>
2024-05-03 23:18:07 +0400

    initial

---
+++ create_video.py
@@ -0,0 +1,144 @@
+import os
+import subprocess
+from PIL import Image, ImageDraw, ImageFont
+import shutil
+import os
+import subprocess
+import shutil
+
+# Specify the folder paths
+cpu_logs_path = 'data/cpu_processes_log'
+output_folder_path = 'data/cpu_logs_img'
+stacked_folder_1_path = 'data/cpu_stack_img_1'
+timestamp_folder_path = 'data/timestamp_img'
+stacked_folder_2_path = 'data/cpu_stack_img_2'
+cpu_total_plot_path = 'data/cpu-total.png'
+output_video_path = 'cpu.mp4'
+cpu_proc_sampling_rate = 1
+
+def create_output_folders(output_folder_path, stacked_folder_1_path, timestamp_folder_path):
+    if os.path.exists(output_folder_path):
+        shutil.rmtree(output_folder_path)
+
+    if os.path.exists(stacked_folder_1_path):
+        shutil.rmtree(stacked_folder_1_path)
+
+    if os.path.exists(timestamp_folder_path):
+        shutil.rmtree(timestamp_folder_path)
+
+    if os.path.exists(stacked_folder_2_path):
+        shutil.rmtree(stacked_folder_2_path)
+
+    if os.path.exists(stacked_folder_2_path):
+        shutil.rmtree(stacked_folder_2_path)
+
+    os.makedirs(output_folder_path)
+    os.makedirs(stacked_folder_1_path)
+    os.makedirs(timestamp_folder_path)
+    os.makedirs(stacked_folder_2_path)
+
+def generate_text_image(lines, output_image_path):
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 15)
+
+    img = Image.new('RGB', (500, 300), color=(73, 109, 137))
+    d = ImageDraw.Draw(img)
+
+    x = 10
+    y = 10
+
+    # Draw each line separately
+    for line in lines:
+        d.text((x, y), line, fill=(255, 255, 0), font=font)
+        y += 15  # Move to the next line. Adjust the value as needed.
+
+    img = img.resize((640, 480))
+
+    img.save(output_image_path)
+
+def concatenate_horizontal(img1, img2, output_image_path):
+    command = [
+        'ffmpeg',
+        '-i', img1,
+        '-i', img2,
+        '-filter_complex', 'hstack',
+        '-y', output_image_path
+    ]
+
+    with open(os.devnull, 'w') as devnull:
+        subprocess.call(command, stdout=devnull, stderr=devnull)
+
+def concatenate_vertical(img1, img2, output_image_path):
+    command = [
+        'ffmpeg',
+        '-i', img1,
+        '-i', img2,
+        '-filter_complex', 'vstack',
+        '-y', output_image_path
+    ]
+
+    with open(os.devnull, 'w') as devnull:
+        subprocess.call(command, stdout=devnull, stderr=devnull)
+
+def generate_timestamp_image(timestamp, output_image_path):
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 40)
+
+    timestamp_img = Image.new('RGB', (1280, 100), color=(73, 109, 137))
+    d = ImageDraw.Draw(timestamp_img)
+    d.text((500, 20), timestamp, fill=(255, 255, 0), font=font)
+
+    timestamp_img.save(output_image_path)
+
+def concatenate_images_to_video(input_folder, duration_per_image, output_video_path):
+    concat_file_path = os.path.join('data/concat.txt')
+    with open(concat_file_path, 'w') as file:
+        for filename in os.listdir(input_folder):
+            if filename.endswith('.png'):
+                file.write(f"file '{os.path.abspath(os.path.join(input_folder, filename))}'\nduration {duration_per_image}\n")
+
+    command = [
+        'ffmpeg',
+        '-f', 'concat',
+        '-safe', '0',
+        '-i', concat_file_path,
+        '-framerate', '1',
+        '-c:v', 'libx264',
+        '-c:a', 'copy',
+        '-r', '30',
+        '-pix_fmt', 'yuv420p',
+        '-y',
+        output_video_path
+    ]
+
+    with open(os.devnull, 'w') as devnull:
+        subprocess.call(command, stdout=devnull, stderr=devnull)
+
+    os.remove(concat_file_path)
+
+create_output_folders(output_folder_path, stacked_folder_1_path, timestamp_folder_path)
+
+# Iterate over the .txt files in the folder
+for filename in os.listdir(cpu_logs_path):
+    if filename.endswith('.txt'):
+        # Load the text
+        with open(os.path.join(cpu_logs_path, filename), 'r') as file:
+            lines = file.readlines()
+
+        # Generate the text image
+        image_filename = os.path.splitext(filename)[0] + '.png'
+        image_stack_1_path = os.path.join(output_folder_path, image_filename)
+        generate_text_image(lines, image_stack_1_path)
+
+        # Concatenate images
+        stacked_image_path = os.path.join(stacked_folder_1_path, image_filename)
+        concatenate_horizontal(cpu_total_plot_path, image_stack_1_path, stacked_image_path)
+
+        # Generate timestamp image
+        timestamp = os.path.splitext(filename)[0].replace('top-cpu-', '')  # Remove 'top-cpu-' prefix
+        timestamp_image_path = os.path.join(timestamp_folder_path, image_filename)
+        generate_timestamp_image(timestamp, timestamp_image_path)
+
+        stacked_imge_2_path = os.path.join(stacked_folder_2_path, timestamp + '_stack.png')
+        concatenate_vertical(stacked_image_path, timestamp_image_path, stacked_imge_2_path)
+
+# concatenate images to video
+concatenate_images_to_video(stacked_folder_2_path, cpu_proc_sampling_rate, output_video_path)
---
+++ main.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+# sudo apt-get update
+# sudo apt-get install -y fio sysstat gnuplot ffmpeg
+
+MONITORING_DURATION="5"
+DISK_LOAD_DURATION="5"
+TOTAL_CPU_SAMPLING_INTERVAL="1"
+PROCESSES_CPU_SAMPLING_INTERVAL="1"
+IDLE_DURATION="$((MONITORING_DURATION - DISK_LOAD_DURATION))"
+DATA_FOLDER="data"
+TOTAL_CPU_TXT_PATH="$DATA_FOLDER/cpu-total.txt"
+TOTAL_CPU_PNG_PATH="$DATA_FOLDER/cpu-total.png"
+PROCESSES_CPU_FOLDER_PATH="$DATA_FOLDER/cpu_processes_log"
+
+echo
+echo "**************"
+echo "Test start: $(date +%T)"
+echo "Monitoring duration: $(( MONITORING_DURATION / 60)) min ($(( IDLE_DURATION / 60)) min without disk load)"
+
+# Function to run program to load the disk
+run_disk_load() {
+    cd data
+    local data_folder="$1"
+    fio --name=write_test \
+        --ioengine=sync \
+        --iodepth=64 \
+        --rw=write \
+        --bs=4k \
+        --size=5G \
+        --numjobs=1 \
+        --runtime="$DISK_LOAD_DURATION" \
+        --time_based \
+        --group_reporting > /dev/null
+    cd ..
+}
+
+# Function to collect total CPU usage using sar
+collect_total_cpu_metrics() {
+    local monitoring_duration="$1"
+    local sampling_interval="$2"
+    local output_txt_path="$3"
+    local number_of_samples="$((monitoring_duration / sampling_interval))"
+    sar -u "$sampling_interval" "$number_of_samples" | grep -v -E "CPU|Average|^$" > $output_txt_path
+}
+
+# Function to collect top CPU processes
+monitor_top_cpu_processes() {
+    local monitoring_duration="$1"
+    local sampling_interval="$2"
+    local output_dir="$3"
+    local top_n_processes=18
+
+    start_time=$(date +%s)
+    current_time=$(date +%s)
+
+    # Recreate the output directory on every script start
+    rm -rf "$output_dir"
+    mkdir -p "$output_dir"
+     
+    while ((current_time - start_time < monitoring_duration)); do
+        # Run the command to get top CPU processes and save it to a text file
+        ps -eo pcpu,pid,user,args --sort=-pcpu \
+            | awk '{cmd=""; for(i=4;i<=NF;i++){cmd=cmd" "$i}; cmd=substr(cmd,1,20); printf "%-8s %-8s %-8s %-20s\n", $1, $2, $3, cmd}' \
+            | head -n "$top_n_processes" > "$output_dir/top-cpu-$(date +%H-%M-%S).txt"
+        sleep 1
+        current_time=$(date +%s)
+    done
+}
+
+# Function to generate CPU usage plot from sar output
+generate_total_cpu_plot() {
+    local total_cpu_txt_path="$1"
+    local total_cpu_png_path="$2"
+    gnuplot -persist << EOF
+    reset
+    # Graph terminal and general config
+    set terminal pngcairo enhanced font 'Verdana,8'
+    set output "$total_cpu_png_path"
+    set title "CPU usage graph"
+    set key bmargin
+    # Styles for different lines
+    set style line 1 lc rgb '#e74c3c' pt 1 ps 1 lt 1 lw 2 # line1
+    set style line 2 lc rgb '#3498db' pt 6 ps 1 lt 1 lw 2 # line2
+    # Axis configuration
+    set style line 11 lc rgb '#2c3e50' lt 1 lw 1.5 # Axis line
+    set border 3 back ls 11
+    set tics nomirror
+    set autoscale xy
+    set xdata time
+    set timefmt "%H:%M:%S"
+    set format x "%H:%M"
+    set xlabel "Time"
+    set ylabel "CPU %"
+    # Background grid
+    set style line 11 lc rgb '#aeb6bf' lt 0 lw 2
+    set grid back ls 11
+    # Begin plotting
+    plot "$total_cpu_txt_path" using 1:3 title 'User%' with l ls 1, \
+        ''                   using 1:5 title 'System%' with l ls 2
+EOF
+}
+
+rm -rf "$DATA_FOLDER"
+mkdir -p "$DATA_FOLDER"
+
+run_disk_load $DATA_FOLDER 
+
+collect_total_cpu_metrics "$MONITORING_DURATION" "$TOTAL_CPU_SAMPLING_INTERVAL" $TOTAL_CPU_TXT_PATH &
+
+monitor_top_cpu_processes "$MONITORING_DURATION" "$PROCESSES_CPU_SAMPLING_INTERVAL" $PROCESSES_CPU_FOLDER_PATH
+
+sleep 1
+
+generate_total_cpu_plot $TOTAL_CPU_TXT_PATH $TOTAL_CPU_PNG_PATH
+
+echo "Test end: $(date +%H:%M:%S)"
+echo "Result CPU usage plot: $(pwd)/cpu.png"
+echo "Creating a video from the CPU logs..."
+
+python create_video.py
+
+rm -rf data
